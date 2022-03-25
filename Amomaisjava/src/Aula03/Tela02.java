5b487032fc77daa1e579eee2086532c57a5e3acf
logonucd <logonucd@UCD-LAB06-27.cds.tat.lab>
2022-03-25 19:24:57 -0300

    teste de acesso

---
+++ Amomaisjava/src/Aula03/Tela02.java
@@ -0,0 +1,48 @@
+package Aula03;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class Tela02 extends JFrame {
+
+	private JPanel contentPane;
+	private JLabel lblTeste;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Tela02 frame = new Tela02();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Tela02() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		lblTeste = new JLabel("Teste");
+		lblTeste.setBounds(171, 110, 46, 14);
+		contentPane.add(lblTeste);
+	}
+
+}
